20d9bab1a3ab79328ed59a61fa788a125e356c42
Tracy Huang <tmacmm@gmail.com>
2020-03-27 20:14:02 +0800

    fix favicon

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,6 +1,7 @@
 $(function(){
 // Document is ready
   var audio = document.getElementsByTagName("audio")[0];
+  audio.volume = 0.5;
   $("#nav-placeholder").load("nav.html").delegate("#sugoi","mouseenter",function(){
     audio.play();
   });
