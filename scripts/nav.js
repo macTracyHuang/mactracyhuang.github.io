5ad13b991ddb7db4a21d8ef80a09957a36c2f9d7
Tracy Huang <tmacmm@gmail.com>
2020-03-27 18:53:02 +0800

    fix sugoi

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,9 +1,10 @@
 $(function(){
 // Document is ready
-$("#nav-placeholder").load("nav.html");
+  $("#nav-placeholder").load("nav.html");
 
-var audio = document.getElementsByTagName("audio")[0];
-$("#sugoi").mouseenter(function() {
-  audio.play();
-});
+  var audio = document.getElementsByTagName("audio")[0];
+  $("#sugoi").mouseenter(function() {
+    audio.play();
+  });
+  alert(audio.name);
 });
