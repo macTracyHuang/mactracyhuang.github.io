f1628a4ffe914429c9d5b77c773f4cac8a90397e
Tracy Huang <tmacmm@gmail.com>
2020-03-27 21:21:40 +0800

    fix volume

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,9 +3,9 @@ $(function(){
   var audio = document.getElementsByTagName("audio")[0];
   var vol = audio.volume;
   vol = 0.2;
+  audio.volume=vol;
   $("#nav-placeholder").load("nav.html").delegate("#sugoi","mouseenter",function(){
     console.log(vol);
-    audio.volume=vol;
     audio.play();
   });
   // $("#sugoi").mouseenter(function() {
